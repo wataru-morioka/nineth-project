693e9baf6060773f61ea9c9c85f1035473df55e9
WataruMorioka <wtr_morioka@icloud.com>
2019-09-07 16:42:25 +0900

    テーブル定義修正

--- serviceApp/models.py
+++ serviceApp/models.py
@@ -7,17 +7,17 @@ class User(models.Model):
         db_table = 'users'
 
 class Account(models.Model):
-    uid = models.CharField(max_length=64, unique=True, null=False, default='')
+    uid = models.CharField(db_index=True, max_length=64, unique=True, null=False, default='')
     account = models.EmailField(max_length=255, null=False, default='')
     delete_flag = models.BooleanField(null=False, default=False)
     webrtc_flag = models.BooleanField(null=False, default=False)
     admin_flag = models.BooleanField(null=False, default=False)
     name = models.CharField(max_length=64, null=False)
     state = models.CharField(max_length=16, null=True)
-    login_count = models.IntegerField(null=False, default=1)
-    latest_login = models.DateTimeField(null=False)
-    created_datetime = models.DateTimeField(null=False)
-    modified_datetime = models.DateTimeField(null=False)
+    login_count = models.IntegerField(db_index=True, null=False, default=1)
+    latest_login = models.DateTimeField(db_index=True, null=False)
+    created_datetime = models.DateTimeField(db_index=True, null=False)
+    modified_datetime = models.DateTimeField(db_index=True, null=False)
 
 
     class Meta:
